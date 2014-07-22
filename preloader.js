143021f0350a98eeea33806fc5f18d83c1a600d2
ryanknights <hello@ryanknights.co.uk>
2014-07-22 21:55:01 +0100

    [Refactored loading events, added progress events]

--- preloader.js
+++ preloader.js
@@ -1,5 +1,12 @@
 PreLoader = (function (window, document)
 {	
+	// ==================================================================================================================
+    // Helper Functions
+    // ==================================================================================================================
+
+	// =========================================================
+	// Extend Objects || Deep Copy
+
 	var extend = function (source)
 	{	
 		var objs = Array.prototype.slice.call(arguments, 1);
@@ -27,10 +34,17 @@ PreLoader = (function (window, document)
 		return source;
 	};
 
+	// =========================================================
+	// Param Is A Function?
+
 	var isFunction = function (fn)
 	{
 		return (typeof fn === 'function')
 	};
+
+	// ==================================================================================================================
+    // PreLoader Constructor
+    // ==================================================================================================================
 
 	var PreLoader = function (images, params)
 	{	
@@ -42,31 +56,55 @@ PreLoader = (function (window, document)
 		this.images  = this._createImageArray(images);
 		this.options = extend({}, this._defaultOptions, params);
 
+		this.counter      = 0;
+		this.imagesLength = this.images.length;
+
+		this.loadedImages  = [];
+		this.errorImages   = [];
+
 		this._init();
 	};
+
+	// ==================================================================================================================
+    // Methods/Properties
+    // ==================================================================================================================
+
+	// =========================================================
+	// Default Options
 
 	PreLoader.prototype._defaultOptions =
 	{
 		complete  : null,
-		eachImage : null,
-		error     : null
+		loaded    : null,
+		error     : null,
+		start     : null
 	};
+
+	// =========================================================
+	// Initialise
 
 	PreLoader.prototype._init = function ()
 	{
 		if (this.images.length > 0)
-		{
+		{	
+			if (isFunction(this.options.start))
+			{
+				this.options.start.call(this, this.images);
+			}
+
 			this._preload();
 		}
 		else
 		{
 			if (isFunction(this.options.complete))
 			{
-				this.options.complete.call(this, this.images);
+				this.options.complete.call(this, this.images, this.loadedImages, this.errorImages);
 			}
 		}
 	};
 
+	// =========================================================
+	// Create Image Array
 
 	PreLoader.prototype._createImageArray = function (images)
 	{
@@ -90,52 +128,75 @@ PreLoader = (function (window, document)
 		}
 
 		return res;
-	}
+	};
+
+	// =========================================================
+	// Check If Loading Is Complete 
+
+	PreLoader.prototype._isComplete = function ()
+	{
+		return this.counter === this.imagesLength;
+	};
+
+	// =========================================================
+	// Return Progress
+
+	PreLoader.prototype._checkProgress = function ()
+	{
+		return Math.round(((this.counter + 1) / this.imagesLength) * 100);
+	};
+
+	// =========================================================
+	// Image Load Events
+
+	PreLoader.prototype._preLoadEvent = function (action, image)
+	{
+		this.counter++;
+
+		this[action + 'Images'].push(image);
+
+		if (isFunction(this.options[action]))
+		{
+			this.options[action].call(this, image, this._checkProgress());
+		}
+
+		if (this._isComplete())
+		{
+			if (isFunction(this.options.complete))
+			{
+				this.options.complete.call(this, this.images, this.loadedImages, this.errorImages);
+			}
+		}
+	};
+
+	// =========================================================
+	// Preload Images
 
 	PreLoader.prototype._preload = function ()
 	{	
-		var	self     = this,
-			complete = 0;
+		var	self = this;
 
-		for (var i = 0, l = this.images.length; i < l; i++)
+		for (var i = 0; i < this.imagesLength; i++)
 		{	
 			(function (img)
 			{
 				var image = new Image();
 
 				image.onload = function ()
-				{	
-					complete++;
-
-					if (isFunction(self.options.eachImage))
-					{
-						self.options.eachImage.call(img, self.images);	
-					}
-
-					if (complete === l)
-					{
-						if (isFunction(self.options.complete))
-						{
-							self.options.complete.call(self, self.images)
-						}
-					}
+				{
+					self._preLoadEvent('loaded', img);
 				}
 
 				image.onerror = function ()
 				{
-					complete++;
-
-					if (isFunction(self.options.error))
-					{
-						self.options.error.call(img, self.images);
-					}
+					self._preLoadEvent('error', img)				
 				}
 
-				image.src = img.getAttribute('data-preload');
+				image.src = img.getAttribute('data-preloader');
 
 			})(this.images[i]);
 		}
-	}
+	};
 
 	return PreLoader;
 
